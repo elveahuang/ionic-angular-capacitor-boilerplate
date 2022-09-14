f0bed56224acf6c2398e0b0d7057899223a9d4c5
Elvea Huang <elvea.huang@gmail.com>
2022-09-14 20:45:11 +0800

    解决VConsole引入的问题

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import VConsole from 'vconsole';
 import { AppModule } from '@/app/app.module';
 import { environment } from '@/environments/environment';
 
 if (environment.production) {
     enableProdMode();
-} else {
 }
+
+new VConsole();
 
 platformBrowserDynamic()
     .bootstrapModule(AppModule)
