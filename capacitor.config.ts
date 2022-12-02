6a9377a84ceac4f7dccdc6f8639d4e629c4e3f5b
Elvea Huang <elvea.huang@gmail.com>
2022-12-02 22:10:20 +0800

    一直在整理，一直在重构，不是在整理就是在重构。

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
     appId: 'cn.elvea.app.capacitor',
-    appName: 'ionic-angular-boilerplate',
+    appName: 'App',
     webDir: 'www',
     bundledWebRuntime: false,
 };
